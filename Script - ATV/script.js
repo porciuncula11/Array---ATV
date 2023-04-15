1631a336e047e212156070fad3e484b4c6add170
porciuncula11 <108681942+porciuncula11@users.noreply.github.com>
2023-04-15 11:30:20 -0300

    Add files via upload

---
+++ Script - ATV/script.js
@@ -0,0 +1,20 @@
+// Escreva um script que remova o último elemento de um array
+
+//const names = ['Melanie', 'Danilo', 'Jennifer'];
+//names.push("Maria");
+//console.log(names);
+
+
+// Escreva um script que adiciona um novo elemento no início 
+// do array
+
+//const animal = ['leão', 'passarinho', 'águia', 'raposa'];
+//animal.pop();
+//console.log(animal);
+
+
+// Escreva um script que adiciona um elmento no final do array
+
+//const fruit = ['jaca', 'pinha', 'morango'];
+//fruit.unshift('uva');
+//console.log(fruit);
